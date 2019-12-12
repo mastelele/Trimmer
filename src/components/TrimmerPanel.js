8a521fbbc5868060c3ea3dc8d4a24435f263503c
masterlele <hovnersisyan@gmail.com>
2019-12-12 06:12:35 +0400

    Added tooltip. Added tick styles.

--- src/components/TrimmerPanel.js
+++ src/components/TrimmerPanel.js
@@ -2,15 +2,24 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { connect } from 'react-redux'
 import { func, number } from 'prop-types'
-import { Range } from 'rc-slider'
+import Slider from 'rc-slider'
 import { setRangeAction } from '../actions/main'
 import 'rc-slider/assets/index.css'
 
+const { createSliderWithTooltip } = Slider
+const Range = createSliderWithTooltip(Slider.Range)
+
 const dotStyle = {
-  color: 'blue',
   width: '1px',
   height: '10px',
   bottom: '0px',
+  borderRadius: 0,
+  border: '1px solid #e9e9',
+}
+
+const tipProps = {
+  placement: 'bottom',
+  prefixCls: 'rc-slider-tooltip',
 }
 
 const TrimmerPanel = ({ duration, start, end, setRange }) => {
@@ -18,10 +27,21 @@ const TrimmerPanel = ({ duration, start, end, setRange }) => {
     const [startPosition, endPosition] = position
     setRange(startPosition, endPosition)
   }
+
+  const tipFormatter = () => {
+    const startSeconds = start / 10
+    const endSeconds = end / 10
+    const startMinutes = Math.floor(startSeconds / 60)
+    const endMinutes = Math.floor(endSeconds / 60)
+    return `${startMinutes}:${startSeconds % 60} - ${endMinutes}:${endSeconds}`
+  }
+
   return (
     <Container>
       <Range
+        tipProps={tipProps}
         dotStyle={dotStyle}
+        tipFormatter={tipFormatter}
         min={0}
         max={duration / 0.1}
         dots
